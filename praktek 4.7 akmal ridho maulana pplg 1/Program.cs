e3e4be698c1d2916287ed530df2830d82fcdbaa2
akmal3143 <akmal258707@student.smkn1kandeman.sch.id>
2025-10-13 13:45:10 +0700

    Add files via upload

---
+++ praktek 4.7 akmal ridho maulana pplg 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktek_4._7_akmal_ridho_maulana_pplg_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("masukkan nilai : ");
+            int nilai = int .Parse(Console.ReadLine());
+
+            if (nilai >= 90)
+            {
+                Console.WriteLine("predikat A");
+
+            }
+            else if (nilai >= 75)
+            {
+                Console.WriteLine("predikat B");
+            }
+            else if (nilai >= 60)
+            {
+                Console.WriteLine("peredikat C");
+            }
+            else
+            {
+                Console.WriteLine("predikat D");
+            }
+        }
+    }
+}
