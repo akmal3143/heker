3fe4fb2de1400ff9398a973e6a1681863875de59
akmal3143 <akmal258707@student.smkn1kandeman.sch.id>
2025-10-13 13:44:34 +0700

    Add files via upload

---
+++ praktek 4.6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktek_4._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("masukkan nilai kamu : ");
+            int nilai = int.Parse(Console.ReadLine());
+
+            if (nilai >= 75)
+            {
+                Console.WriteLine("kamu lulus");
+            }
+            else
+            {
+                Console.WriteLine("kamu tidak lulus");
+            }
+        }
+    }
+}
